a6d32f1819c32c559223efab055cc53cdf56a989
Genaro Hernandez Reyes <genarohr@outlook.com>
2018-07-09 02:41:54 -0500

    xls refractored

--- MagicInfoTest1/Form1.cs
+++ MagicInfoTest1/Form1.cs
@@ -16,8 +16,7 @@ namespace MagicInfoTest1
 {
     public partial class Form1 : Form
     {
-        private string Excel03ConString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source={0};Extended Properties='Excel 8.0;HDR={1}'";
-        private string Excel07ConString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Extended Properties='Excel 8.0;HDR={1}'";
+
         public static string selectedPromo;
 
         private string path = "";
@@ -103,58 +102,18 @@ namespace MagicInfoTest1
         private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
         {
             string filePath = openFileDialog1.FileName;
-            string extension = Path.GetExtension(filePath);
-            string header = "YES";
-            string conStr, sheetName;
+            xlsRead xr = new xlsRead();
+            DataTable dt = new DataTable();
 
-            conStr = string.Empty;
-            switch (extension)
+            try
             {
-
-                case ".xls": //Excel 97-03
-                    conStr = string.Format(Excel03ConString, filePath, header);
-                    break;
-
-                case ".xlsx": //Excel 07
-                    conStr = string.Format(Excel07ConString, filePath, header);
-                    break;
+                dt = xr.readXLS(filePath);
+            }
+            catch
+            {
+                MessageBox.Show("No compatible data");
             }
 
-            //Get the name of the First Sheet.
-           
-            using (OleDbConnection con = new OleDbConnection(conStr))
-            {
-                using (OleDbCommand cmd = new OleDbCommand())
-                {
-                    cmd.Connection = con;
-
-                    con.Open();
-                    DataTable dtExcelSchema = con.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
-                    sheetName = dtExcelSchema.Rows[0]["TABLE_NAME"].ToString();
-                    con.Close();
-                }
-            }
-
-            //Read Data from the First Sheet.
-            using (OleDbConnection con = new OleDbConnection(conStr))
-            {
-                using (OleDbCommand cmd = new OleDbCommand())
-                {
-                    using (OleDbDataAdapter oda = new OleDbDataAdapter())
-                    {
-                        DataTable dt = new DataTable();
-                        cmd.CommandText = "SELECT * From [" + sheetName + "]";
-                        cmd.Connection = con;
-                        con.Open();
-                        oda.SelectCommand = cmd;
-                        oda.Fill(dt);
-                        con.Close();
-
-                        //Populate DataGridView.
-                        dataGridView1.DataSource = dt;
-                    }
-                }
-            }
         }
 
 
---
+++ MagicInfoTest1/xlsRead.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicInfoTest1
+{
+    class xlsRead
+    {
+        private string Excel03ConString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source={0};Extended Properties='Excel 8.0;HDR={1}'";
+        private string Excel07ConString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Extended Properties='Excel 8.0;HDR={1}'";
+
+        public DataTable readXLS(string filePath) {
+
+            string extension = Path.GetExtension(filePath);
+            string header = "YES";
+            string conStr, sheetName;
+
+            conStr = string.Empty;
+            switch (extension)
+            {
+
+                case ".xls": //Excel 97-03
+                    conStr = string.Format(Excel03ConString, filePath, header);
+                    break;
+
+                case ".xlsx": //Excel 07
+                    conStr = string.Format(Excel07ConString, filePath, header);
+                    break;
+            }
+
+            //Get the name of the First Sheet.
+            using (OleDbConnection con = new OleDbConnection(conStr))
+            {
+                using (OleDbCommand cmd = new OleDbCommand())
+                {
+                    cmd.Connection = con;
+
+                    con.Open();
+                    DataTable dtExcelSchema = con.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+                    sheetName = dtExcelSchema.Rows[0]["TABLE_NAME"].ToString();
+                    con.Close();
+                }
+            }
+
+            //Read Data from the First Sheet.
+            using (OleDbConnection con = new OleDbConnection(conStr))
+            {
+                using (OleDbCommand cmd = new OleDbCommand())
+                {
+                    using (OleDbDataAdapter oda = new OleDbDataAdapter())
+                    {
+                        DataTable dt = new DataTable();
+                        cmd.CommandText = "SELECT * From [" + sheetName + "]";
+                        cmd.Connection = con;
+                        con.Open();
+                        oda.SelectCommand = cmd;
+                        oda.Fill(dt);
+                        con.Close();
+
+                        //Populate DataGridView.
+                        return dt;
+                    }
+                }
+            }
+
+
+        }
+
+    }
+}
